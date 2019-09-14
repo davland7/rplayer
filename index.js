2bab0751f23fa5f1af9d37cc2952910d861b37e0
davland7 <davland7@gmail.com>
2019-09-14 13:48:54 -0400

    Update website

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 if (!('ontouchstart' in document.documentElement)) {
   document.documentElement.className += 'no-touch';
 }
+
+window['__onGCastApiAvailable'] = function(isAvailable) {
+  if (isAvailable) {
+    initializeCastApi();
+  }
+};
+
+initializeCastApi = function() {
+  cast.framework.CastContext.getInstance().setOptions({
+    receiverApplicationId: '8DF59165',
+    autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED
+  });
+};
