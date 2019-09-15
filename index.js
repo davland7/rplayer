9d95911fed724743c8cf5dc0ff8eb2a7807fc5e0
David Landry <davland7@gmail.com>
2019-09-15 08:54:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ initializeCastApi = function() {
     autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED
   });
 };
+
+var castSession = cast.framework.CastContext.getInstance().getCurrentSession();
+
+var mediaInfo = new chrome.cast.media.MediaInfo(currentMediaURL, contentType);
+var request = new chrome.cast.media.LoadRequest(mediaInfo);
+castSession.loadMedia(request).then(
+  function() { console.log('Load succeed'); },
+  function(errorCode) { console.log('Error code: ' + errorCode); });
+
+var playerCast = new cast.framework.RemotePlayer();
+var playerController = new cast.framework.RemotePlayerController(playerCast);
+
+playerController.playOrPause();
