75eb094db55b7e430ecf54e3018b61fd768c3878
David Landry <davland7@gmail.com>
2019-11-21 11:16:38 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,16 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('rp-20191121').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/?source=pwa',
+       'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js',
+       '/dist/rplayer.js'
+     ]);
+   })
+ );
+});
 
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+});
