4bea6e296bb2d83730b0cae18636f0649cd3d347
David Landry <davland7@gmail.com>
2019-11-21 12:15:41 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-window.addEventListener('beforeinstallprompt', (e) => {
+self.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 76 and later from showing the mini-infobar
   e.preventDefault();
   // Stash the event so it can be triggered later.
