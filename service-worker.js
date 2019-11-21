57e38e46e7756e838b6962fbbd9136ce55fad63e
David Landry <davland7@gmail.com>
2019-11-21 12:13:25 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,12 @@
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 76 and later from showing the mini-infobar
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  showInstallPromotion();
+});
+
+/*
 self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('rp-20191122').then(function(cache) {
@@ -10,3 +19,4 @@ self.addEventListener('install', function(e) {
    })
  );
 });
+*/
