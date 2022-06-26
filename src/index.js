baee889888c5cb055047f2a6a381df501b45d056
davland7 <davland7@gmail.com>
2022-06-26 09:23:12 -0400

    Change for playSrc

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ export default class rPlayer extends Audio {
     constructor() {
         super();
 
+        const key = 'rplayer-volume';
         this.hls = null;
 
-        if (localStorage.hasOwnProperty('r-player2-volume')) {
-            this.volume = localStorage.getItem('r-player2-volume');
+        if (localStorage.hasOwnProperty(key)) {
+            this.volume = localStorage.getItem(key);
         } else {
             this.volume = .2;
         }
 
-        this.onvolumechange = function() {
-            localStorage.setItem('r-player2-volume', Number(this.volume)).toFixed(1);
+        this.onvolumechange = () => {
+            localStorage.setItem(key, this.volume);
         };
     }
 
@@ -63,18 +64,12 @@ export default class rPlayer extends Audio {
         }
     }
 
-    // event volume
-
     upVolume() {
-        const value = this.volume;
-
-        this.#setVolume(value + 0.1);
+        this.#setVolume(this.volume + 0.1);
     }
 
     downVolume() {
-        const value = this.volume;
-
-        this.#setVolume(value - 0.1);
+        this.#setVolume(this.volume - 0.1);
     }
 
     /**
@@ -84,8 +79,6 @@ export default class rPlayer extends Audio {
     #setVolume(value) {
         if (value >= 0.0 && value <= 1.0) {
             this.volume = Number(value).toFixed(1);
-            // probleme localStorage
-            localStorage.setItem('r-player2-volume', Number(value).toFixed(1));
         }
     }
 
@@ -102,7 +95,7 @@ export default class rPlayer extends Audio {
             return this.src;
         }
     }
-    // true|false
+
     get playing() {
         return this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2;
     }
