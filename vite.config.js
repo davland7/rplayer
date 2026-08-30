0d8a9482150e5ddabd46a4dab37ae3b555476bf2
davland7 <davland7@gmail.com>
2026-08-30 12:09:54 -0400

    feat: enhance build process for demo mode and update .gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,32 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const isDemo = mode === "demo";
+
   return {
+    base: "./",
+    root: isDemo ? "demo" : ".",
     publicDir: false,
-    build: {
-      lib: {
-        entry: 'src/index.js',
-        name: 'RPlayer',
-        fileName: (format) => {
-          if (format === 'umd') return 'rplayer.umd.js';
-          if (format === 'iife') return 'rplayer.min.js';
-          return 'rplayer.js';
+    build: isDemo
+      ? {
+          outDir: "../docs",
+          emptyOutDir: true,
+          assetsDir: "assets",
+          rollupOptions: {
+            input: "index.html",
+          },
+        }
+      : {
+          lib: {
+            entry: "src/index.js",
+            name: "RPlayer",
+            fileName: (format) => {
+              if (format === "umd") return "rplayer.umd.js";
+              if (format === "iife") return "rplayer.min.js";
+              return "rplayer.js";
+            },
+            formats: ["es", "umd"],
+          },
         },
-        formats: ['es', 'umd']
-      },
-    }
   };
 });
